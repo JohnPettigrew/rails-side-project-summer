3629334ae118e80e50866112450f2acea73cf2e8
John Pettigrew <john@wearefutureproofs.com>
2019-04-18 09:00:25 +0100

    Fix header for signed in/out users
    And remove default test fixtures for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "If you're seeing this, something has gone badly wrong!"
   end
 end
