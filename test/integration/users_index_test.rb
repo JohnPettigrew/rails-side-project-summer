36d79dea7828dee53b01aeacbb7825cdf2206ba1
John Pettigrew <john@wearefutureproofs.com>
2019-04-20 16:19:07 +0100

    Remove non-functional test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -7,11 +7,4 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_redirected_to new_user_session_path
   end
 
-#   Currently can't log in, so not worth working on until that process works in tests
-#   test "Users index page logged in" do
-#     get users_path
-#     assert_response :success
-#     assert_template 'layouts/users/index'
-#     assert_select "a[href=?]", user_path, count: 1
-#   end
 end
