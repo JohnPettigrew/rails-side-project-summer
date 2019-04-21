d5d0a5184c2aac92a29e1c3bf99b33710562865a
John Pettigrew <john@wearefutureproofs.com>
2019-04-21 16:31:35 +0100

    Bits and pieces

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,28 +3,23 @@ class Ability
 
   def initialize(user)
     user ||= User.new # define user when not logged in
+
     if user.id # logged in
-      can :manage, :all
+      can :manage, User
+#      can :manage, User, :user_id => user.id
     end
 
     can [:read, :show], :all
+  end
 
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
+  def initialize(project)
+    project ||= Project.new
+
+    if project.id
+      can :read, Project
+      can :manage, Project, :project_id => project.id
+    end
+
+    can [:read, :show], :all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :projects, dependent: :destroy
+  has_many :projects, inverse_of: :user, dependent: :destroy
   accepts_nested_attributes_for :projects
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
