9151b0aebd556922c9ca99aa2ac60955dff012f9
John Pettigrew <john@wearefutureproofs.com>
2019-04-21 20:16:26 +0100

    Improve user onboarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:index]
 
   def show
     @user = User.find(params[:id])
