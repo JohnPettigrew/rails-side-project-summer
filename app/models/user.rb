65cfbe1716f9d95f5e756ebc10e9f1af6eb0df42
John Pettigrew <john@wearefutureproofs.com>
2019-04-26 17:59:31 +0100

    Setup Twitter
    Encrypted storage of Twitter keys, import omniauth.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :projects, inverse_of: :user, dependent: :destroy
   accepts_nested_attributes_for :projects
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # Others available are :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[twitter]
 end
--- test/integration/users_index_test.rb
+++
@@ -1,5 +0,0 @@
-require 'test_helper'
-
-class UsersIndexTest < ActionDispatch::IntegrationTest
-
-end
