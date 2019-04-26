e86c631351735bb8efb9888d43dc83250ac77a09
John Pettigrew <john@wearefutureproofs.com>
2019-04-26 16:23:42 +0100

    Add more detail to logged-out tests

--- test/integration/logged_out_navigation_test.rb
+++ test/integration/logged_out_navigation_test.rb
@@ -20,18 +20,23 @@ class LoggedOutNavigationTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'users/index'
     assert_select "title", "All participants | #{@base_title}"
+    assert_select ".users>li", minimum: 1
     get projects_path
     assert_response :success
     assert_template 'projects/index'
     assert_select "title", "All projects | #{@base_title}"
+    assert_select ".projects>.project", minimum: 1
     get new_user_session_path
     assert_response :success
     assert_template 'devise/sessions/new'
     assert_select "title", "Sign in | #{@base_title}"
+    assert_select "input", count: 6
     get new_user_registration_path
     assert_response :success
     assert_template 'devise/registrations/new'
     assert_select "title", "Take part! | #{@base_title}"
+    assert_select "input", count: 8
+    assert_select "textarea", count: 1
   end
 
   test "Read project details" do
@@ -39,6 +44,9 @@ class LoggedOutNavigationTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'projects/show'
     assert_select "title", "Project details | #{@base_title}"
+    assert_select ".project-name", count: 1
+    assert_select ".project-description", count: 1
+    assert_select ".project-source", count: 1
     assert_select ".project-listed", count: 1
     assert_select ".project-edit", count: 0
   end
