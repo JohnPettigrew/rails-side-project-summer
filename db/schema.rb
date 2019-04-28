8aa60e31df526475a34802994952dde914f9c07d
John Pettigrew <john@wearefutureproofs.com>
2019-04-28 21:45:47 +0100

    Save Twitter user tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_201239) do
+ActiveRecord::Schema.define(version: 2019_04_28_203114) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_04_26_201239) do
     t.string "provider"
     t.string "uid"
     t.string "twitter_user_url"
+    t.string "twitter_key"
+    t.string "twitter_secret"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
