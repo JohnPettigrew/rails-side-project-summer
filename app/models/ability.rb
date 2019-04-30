01c46125e2bfe97a06ce514b2339d3d20b8da3db
John Pettigrew <john@wearefutureproofs.com>
2019-04-30 17:53:53 +0100

    Add admin accounts and functionality

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here.
-    can :read, User
-    can :read, Project
+    # Define abilities for the passed-in user here.
+    can :read, [User, Project]
 
     if user
       can :create, Project
       can :manage, Project, user_id: user.id
       can :manage, User, id: user.id
+
+      if user.admin?
+        can :manage, Project
+        can :manage, User
+      end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_28_203114) do
+ActiveRecord::Schema.define(version: 2019_04_30_154125) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_04_28_203114) do
     t.string "twitter_user_url"
     t.string "twitter_key"
     t.string "twitter_secret"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
