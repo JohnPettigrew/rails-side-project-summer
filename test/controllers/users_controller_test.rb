7f455d5222b16e46e3e73f19d22c8f05c03d9228
John Pettigrew <john@wearefutureproofs.com>
2019-05-01 13:06:31 +0100

    Tests for admin

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,10 +3,36 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user=users(:twitter)
+    @twitter=users(:twitter)
+    @admin=users(:admin)
+    @user=users(:one)
+  end
+
+  test "admin_destroy works" do
+    sign_in @admin
+    assert_difference 'User.count', -1 do
+      delete admin_destroy_user_path(@user)
+    end
+  end
+
+  test "admin_edit works" do
+    sign_in @admin
+    get admin_edit_user_path(@user.id)
+    assert_response :success
+    @name=@user.name
+    patch admin_patch_user_path(@user), params: {id: @user.id, user: { name: "New name"}}
+    @name2=@user.reload.name
+    assert_not_equal @name, @name2
   end
 
   test "Delete Twitter details works" do
-#    assert_nil @user.twitter_key
+    sign_in @twitter
+    assert_not_nil @twitter.twitter_key
+    get '/twitter_disconnect.html'
+    assert_nil @twitter.twitter_key
+    assert_nil @twitter.twitter_secret
+    assert_nil @twitter.twitter_user_url
+    assert_nil @twitter.uid
+    assert_nil @twitter.provider
   end
 end
---
+++ test/integration/admin_user_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class AdminUserTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "#sideprojectsummer"
+    @admin=users(:admin)
+    @user=users(:one)
+    @project=projects(:orange)
+  end
+
+  test "Can see admin users on index and profiles" do
+    sign_in @admin
+    get users_path
+    assert_response :success
+    assert_select ".user-name", title=@admin.name + " (is an admin)", count: 1
+    get user_path(@admin)
+    assert_response :success
+    assert_select ".profile-name", title=@admin.name + " (is an admin)", count: 1
+    assert_select ".user-admin", count: 1
+  end
+
+  test "Can delete users from user index" do
+    sign_in @admin
+    get users_path
+    assert_response :success
+    assert_select ".delete-user-link", count: 4
+    assert_select "div.delete-user-link>a[data-method=?]", "delete", count: 4
+  end
+
+  test "Can edit other users from their profiles" do
+  end
+
+  test "Can edit other users' projects" do
+  end
+end
